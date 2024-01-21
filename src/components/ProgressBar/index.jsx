87545f3fa9d2794c9e6c89b4f06f122e5b9e93d2
Max <agafouov@gmail.com>
2024-01-21 05:20:40 +0000

    Updates

--- src/components/ProgressBar/index.jsx
+++
@@ -1,9 +0,0 @@
-import "./style.scss"
-
-export default function ProgressBar({max}) {
-    return(
-        <progress max={max} value={5000} className="progress-bar">
-
-        </progress>
-    )
-}
--- src/utils/getRandomKey.js
+++
@@ -1,1 +0,0 @@
-export const getRandomKey = () => Math.random().toString(36).substring(2, 15);
